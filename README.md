9083b07b879b72d35f4e800bff0dce4455f451be
Andrew-157 <subotinandrey5@gmail.com>
2026-01-04 10:44:55 +0100

    exemplary failing service

--- README.md
+++ README.md
@@ -26,3 +26,11 @@ when compiling the program, you need to link systemd package
 gcc -Wall watchdog_script.c -lsystemd
 ```
 
+## sd_notify WATCHDOG=1
+
+`watchdog_script.c` in this repo is the script that could be used in systemd service with [WatchdogSec=](https://www.freedesktop.org/software/systemd/man/latest/systemd.service.html?__goaway_challenge=meta-refresh&__goaway_id=8d1f640f98bf4ab32de4c7f69ef37ce8&__goaway_referer=https%3A%2F%2Fwww.google.com%2F#WatchdogSec=) set, that is, the main process or forked processes (depending on [NotifyAccess=](https://www.freedesktop.org/software/systemd/man/latest/systemd.service.html?__goaway_challenge=meta-refresh&__goaway_id=8d1f640f98bf4ab32de4c7f69ef37ce8&__goaway_referer=https%3A%2F%2Fwww.google.com%2F#NotifyAccess=) set in the service definition) must call `sd_notify` function with `WATCHDOG=1` every <= `WatchdogSec=`, otherwise the process will get `SIGABRT` and the subsequent behavior of systemd is defined by [Restart=](https://www.freedesktop.org/software/systemd/man/latest/systemd.service.html?__goaway_challenge=meta-refresh&__goaway_id=8d1f640f98bf4ab32de4c7f69ef37ce8&__goaway_referer=https%3A%2F%2Fwww.google.com%2F#WatchdogSec=) option in the service definition.
+
+If `FAIL_WATCHDOG` environment variable is not set or set to any other value than "1", `watchdog_script.c` will call `sd_notify` every `WatchdogSec` - 1 seconds, and systemd will not terminate the script with `SIGABRT`, the exemplary service is in srv/watchdog.service.
+
+If `FAIL_WATCHDOG` environment variable is set to "1", `watchdog_script.c` will sleep `WatchdogSec` + 1 seconds between calls to `sd_notify`, which will cause it to get `SIGABRT` from systemd, the exemplary service is in srv/watchdog-fail.service.
+
--- watchdog_script.c
+++ watchdog_script.c
@@ -6,12 +6,16 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <syslog.h>
 #include <systemd/sd-daemon.h>
 #include <unistd.h>
 
 #define NOTIFY_SOCKET_ENV "NOTIFY_SOCKET"
 #define WATCHDOG_USEC_ENV "WATCHDOG_USEC"
+#define USEC_CONVERSION 1000000
+#define FAIL_WATCHDOG_ENV "FAIL_WATCHDOG"  // env to simulate service failure due to too long sleeps between sd_notify calls
+
 
 void signal_handler(int sig) {
         printf("Caught signal: %d\n", sig);
@@ -20,6 +24,11 @@ void signal_handler(int sig) {
 }
 
 int main() {
+        if (getenv(NOTIFY_SOCKET_ENV) == NULL || getenv(WATCHDOG_USEC_ENV) == NULL) {
+                fprintf(stderr, "Either '%s' or '%s' environment variables were not set, exiting with failure\n", NOTIFY_SOCKET_ENV, WATCHDOG_USEC_ENV);
+                exit(1);
+        }
+
         openlog(NULL, 0, LOG_USER);
 
         signal(SIGABRT, signal_handler);
@@ -28,10 +37,22 @@ int main() {
 
     syslog(LOG_INFO, "NOTIFY_SOCKET value: %s\n", getenv(NOTIFY_SOCKET_ENV));
         syslog(LOG_INFO, "WATCHDOG_USEC value: %s\n", getenv(WATCHDOG_USEC_ENV));
+        syslog(LOG_INFO, "FAIL_WATCHDOG value: %s\n", getenv(FAIL_WATCHDOG_ENV));
+
+        int sleep_time;
+        if (getenv(FAIL_WATCHDOG_ENV) != NULL && strcmp(getenv(FAIL_WATCHDOG_ENV), "1") == 0) {
+                syslog(LOG_INFO, "Script will be restarted by systemd due to too long sleeps\n");
+                sleep_time = atoi(getenv(WATCHDOG_USEC_ENV)) / USEC_CONVERSION + 1;
+        } else {
+                syslog(LOG_INFO, "Script will keep running until manually stopped or restarted\n");
+                sleep_time = atoi(getenv(WATCHDOG_USEC_ENV)) / USEC_CONVERSION - 1;  // sleep_time could also be equal to WATCHDOG_USEC_ENV and systemd would not restart the service
+        }
+
+        syslog(LOG_INFO, "Sleep time value: %d\n", sleep_time);
 
         while (1) {
                 syslog(LOG_INFO, "Script is running...\n");
-                sleep(5);
+                sleep(sleep_time);
                 // NOTE: accoring to systemd docs, it is recommended to not check return value of the call to sd_notify: https://www.freedesktop.org/software/systemd/man/latest/sd_notify.html#Return%20Value
                 sd_notify(0, "WATCHDOG=1");
         }
