fcbf0f1b1d6a3427bdb5432f175ad93af85b34c4
Andrew-157 <subotinandrey5@gmail.com>
2026-01-04 11:07:35 +0100

    do not use stdio at all

--- watchdog_script.c
+++ watchdog_script.c
@@ -2,9 +2,8 @@
  *
  * Exemplary script to be used in systemd service with Type=simple, and WatchdogSec= set, e.g. script should send WATCHDOG=1 with sd_notify
  *
- * */
+ */
 #include <signal.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
@@ -18,44 +17,43 @@
 
 
 void signal_handler(int sig) {
-        printf("Caught signal: %d\n", sig);
-        closelog();
-        exit(0);
+	syslog(LOG_INFO, "Caught signal: %d\n", sig);
+    closelog();
+    exit(0);
 }
 
 int main() {
-        openlog(NULL, 0, LOG_USER);
-        if (getenv(NOTIFY_SOCKET_ENV) == NULL || getenv(WATCHDOG_USEC_ENV) == NULL) {
-                syslog(LOG_INFO, "Either '%s' or '%s' environment variables were not set, exiting with failure\n", NOTIFY_SOCKET_ENV, WATCHDOG_USEC_ENV);
-                closelog();
-                exit(1);
-        }
+	openlog(NULL, 0, LOG_USER);
+    if (getenv(NOTIFY_SOCKET_ENV) == NULL || getenv(WATCHDOG_USEC_ENV) == NULL) {
+    	syslog(LOG_INFO, "Either '%s' or '%s' environment variables were not set, exiting with failure\n", NOTIFY_SOCKET_ENV, WATCHDOG_USEC_ENV);
+        closelog();
+        exit(1);
+    }
 
-        signal(SIGABRT, signal_handler);
-        signal(SIGINT, signal_handler);
-        signal(SIGTERM, signal_handler);
+    signal(SIGABRT, signal_handler);
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
 
     syslog(LOG_INFO, "NOTIFY_SOCKET value: %s\n", getenv(NOTIFY_SOCKET_ENV));
-        syslog(LOG_INFO, "WATCHDOG_USEC value: %s\n", getenv(WATCHDOG_USEC_ENV));
-        syslog(LOG_INFO, "FAIL_WATCHDOG value: %s\n", getenv(FAIL_WATCHDOG_ENV));
+    syslog(LOG_INFO, "WATCHDOG_USEC value: %s\n", getenv(WATCHDOG_USEC_ENV));
+    syslog(LOG_INFO, "FAIL_WATCHDOG value: %s\n", getenv(FAIL_WATCHDOG_ENV));
 
-        int sleep_time;
-        if (getenv(FAIL_WATCHDOG_ENV) != NULL && strcmp(getenv(FAIL_WATCHDOG_ENV), "1") == 0) {
-                syslog(LOG_INFO, "Script will be restarted by systemd due to too long sleeps\n");
-                sleep_time = atoi(getenv(WATCHDOG_USEC_ENV)) / USEC_CONVERSION + 1;
-        } else {
-                syslog(LOG_INFO, "Script will keep running until manually stopped or restarted\n");
-                sleep_time = atoi(getenv(WATCHDOG_USEC_ENV)) / USEC_CONVERSION;
-        }
+    int sleep_time;
+    if (getenv(FAIL_WATCHDOG_ENV) != NULL && strcmp(getenv(FAIL_WATCHDOG_ENV), "1") == 0) {
+    	syslog(LOG_INFO, "Script will be restarted by systemd due to too long sleeps\n");
+        sleep_time = atoi(getenv(WATCHDOG_USEC_ENV)) / USEC_CONVERSION + 1;
+    } else {
+        syslog(LOG_INFO, "Script will keep running until manually stopped or restarted\n");
+        sleep_time = atoi(getenv(WATCHDOG_USEC_ENV)) / USEC_CONVERSION;
+    }
 
-        syslog(LOG_INFO, "Sleep time value: %d\n", sleep_time);
+    syslog(LOG_INFO, "Sleep time value: %d\n", sleep_time);
 
-        while (1) {
-                syslog(LOG_INFO, "Script is running...\n");
-                sleep(sleep_time);
-                // NOTE: accoring to systemd docs, it is recommended to not check return value of the call to sd_notify: https://www.freedesktop.org/software/systemd/man/latest/sd_notify.html#Return%20Value
-                sd_notify(0, "WATCHDOG=1");
-        }
+    while (1) {
+    	syslog(LOG_INFO, "Script is running...\n");
+        sleep(sleep_time);
+        sd_notify(0, "WATCHDOG=1");  // NOTE: accoring to systemd docs, it is recommended to not check return value of the call to sd_notify: https://www.freedesktop.org/software/systemd/man/latest/sd_notify.html#Return%20Value
+    }
 
-        closelog();
+	closelog();  // i don't know if it makes sense to call it here, would script ever get here even?
 }
